4a09de4879a2e38d6ffed8ba55c6fdecf4fd9e20
farrelayman09 <farrel.ayman09@gmail.com>
2024-04-27 16:44:24 +0700

    modify tayangan frontend to fit TK3 requirements, make the headings gray & shift search bar to the left

--- tayangan/static/js/script.js
+++ tayangan/static/js/script.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
-    $('#example').DataTable();
+    new DataTable('#example', {
+        layout: {
+            topStart: null,
+            topEnd: null
+        }
+    });
 });
 
 $(document).ready(function() {
-    $('#exampleFilm').DataTable();
+    new DataTable('#exampleFilm', {
+        layout: {
+            topStart: null,
+            topEnd: null
+        }
+    });
 });
 
 $(document).ready(function() {
-    $('#exampleSeries').DataTable();
+    new DataTable('#exampleSeries', {
+        layout: {
+            topStart: null,
+            topEnd: null
+        }
+    });
 });
