928d5e3c3f191a9b308dce53bb6a37bf80edb6cf
farrelayman09 <farrel.ayman09@gmail.com>
2024-05-07 03:07:02 +0700

    add select * from contributors query and show it on page

--- contributor/views.py
+++ contributor/views.py
@@ -1,5 +1,43 @@
-from django.shortcuts import render
+import uuid
+
+from django.db import connection
+from django.shortcuts import render, redirect
+from collections import namedtuple
+
+def map_cursor(cursor):
+    description = cursor.description
+    query_result = namedtuple("Data", [col[0] for col in description])
+    return [query_result(*row) for row in cursor.fetchall()]
+
+# Basically, it returns a list of object/namedtuple. The way we access or retrieve the attributes is the same like OOP.
+def query(query_str: str):
+    result = []
+    with connection.cursor() as cursor:
+        try:
+            cursor.execute(query_str)
+            if query_str.strip().lower().startswith("select"):
+                # Return hasil SELECT
+                result = map_cursor(cursor)
+            else:
+                # Return jumlah row yang termodifikasi oleh INSERT, UPDATE, DELETE (int)
+                result = cursor.rowcount
+        except Exception as e:
+            # Any
+            result = e
+    return result
 
 def show_contributor(request):
-   return render(request, "contributor.html")
+   data = query(
+        f"""
+        SELECT *
+        FROM contributors;
+        """
+    )
+
+   context = {
+        "data": data
+    }
+   print(len(data))
+   
+   return render(request, "contributor.html", context)
 
